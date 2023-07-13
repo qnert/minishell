d3c5a676b0fe760c99304b8aa7b83ac3fe88123f
Simon Kunert <skunert@1-A-6.42heilbronn.de>
2023-07-13 14:15:45 +0200

    short change in structure but no real code changes

--- src/main.c
+++ src/main.c
@@ -6,13 +6,13 @@
 /*   By: skunert <skunert@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/05 12:24:59 by skunert           #+#    #+#             */
-/*   Updated: 2023/07/12 14:40:39 by skunert          ###   ########.fr       */
+/*   Updated: 2023/07/13 14:08:44 by skunert          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-int	main(void)
+void	shell_loop(void)
 {
 	char	*str;
 
@@ -20,9 +20,22 @@ int	main(void)
 	while (str != NULL)
 	{
 		if (!(handle_build_in(str)))
-			return (0);
-		str = readline("miniHell > ");
+			return ;
 		free(str);
+		str = readline("miniHell > ");
 	}
+	free(str);
+	return ;
+}
+
+int	main(int argc, char **argv)
+{
+	if (argc != 1 && argv == NULL)
+	{
+		printf("Wrong amount of arguments!\n");
+		return (-1);
+	}
+	else
+		shell_loop();
 	return (0);
 }
