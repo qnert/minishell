37c61f6d5e20571159c965104703009e11bc3987
Simon Kunert <skunert@1-A-6.42heilbronn.de>
2023-07-25 11:57:14 +0200

    builtin free alloc memory done

--- src/built_in.c
+++ src/built_in.c
@@ -6,7 +6,7 @@
 /*   By: skunert <skunert@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/12 14:39:05 by skunert           #+#    #+#             */
-/*   Updated: 2023/07/25 11:12:59 by skunert          ###   ########.fr       */
+/*   Updated: 2023/07/25 11:56:37 by skunert          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,6 +40,8 @@ void	handle_built_in(t_shell *sh, char *str)
 	if (ft_strncmp(str, "exit", 4) == 0)
 	{
 		terminate_struct(sh);
+		free_arr(sh->envp);
+		free(sh);
 		exit (0);
 	}
 	if (ft_strncmp(str, "env", 3) == 0)
