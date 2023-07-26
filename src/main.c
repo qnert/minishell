8743236d2e1a84354d560fb909a5c33c6719bb95
Simon Kunert <skunert@1-A-6.42heilbronn.de>
2023-07-26 14:36:24 +0200

    .

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: skunert <skunert@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/05 12:24:59 by skunert           #+#    #+#             */
-/*   Updated: 2023/07/26 13:34:49 by skunert          ###   ########.fr       */
+/*   Updated: 2023/07/26 14:35:52 by skunert          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,7 +58,7 @@ void	shell_loop(t_shell *sh)
 				execute_main(sh);
 		}
 		// printf("amount of pipes: %d\n", sh->pipes);
-		print_matrix(sh->cmd_table);
+		// print_matrix(sh->cmd_table);
 		// if (sh->outfiles != NULL)
 		// {
 		// 	printf("outfile: %s\n", sh->outfiles->file_name);
